fd1422b265bed7c860392ce98a29f1f8003ac29a
Syed Muhammad Saqlain Bukhari <saqlainbukhari783@gmail.com>
2026-04-01 09:06:14 +0500

    initial change

--- railway.sh
+++ railway.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Installing Playwright browsers..."
-playwright install --with-deps
+pip install -r requirements.txt && playwright install --with-deps chromium
 
+#!/bin/bash
 echo "Starting app..."
 gunicorn app:app --bind 0.0.0.0:$PORT --log-level debug --access-logfile - --error-logfile -
